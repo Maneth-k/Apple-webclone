7459ba1005737baf6d3b0b54e967b890b585890d
Maneth-k <manethkaveen0@gmail.com>
2025-11-15 23:44:21 +0530

    implemented model switch for macbook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
+import { ScrollTrigger,SplitText } from 'gsap/all'
+import { gsap } from 'gsap'
+gsap.registerPlugin(ScrollTrigger,SplitText)
 const App = () => {
   return (
    <main>
     <NavBar/>
     <Hero/>
+    <ProductViewer/>
    </main>
   )
 }   
